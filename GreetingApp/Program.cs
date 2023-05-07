d3735566dad14d1cd1716f31e1b72844061bc94b
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-06 22:26:56 -0600

    application greets user, asks user for name and responds with given name. application complete.

--- GreetingApp/Program.cs
+++ GreetingApp/Program.cs
@@ -1,11 +1,20 @@
 ﻿
 
 //Welcome User to App
-
+Console.WriteLine("Welcome to the Greeting Application!");
+Console.WriteLine("This application was built by Mohammad Jabir");
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
 
 //Ask for First Name
-
+Console.Write("What is your first name: ");
+string? firstName;
+firstName = Console.ReadLine();
 
 //Greet User by Name
+Console.WriteLine();
+Console.WriteLine("Hello " + firstName);
 
+Console.WriteLine("Thank you for using my application.");
+Console.WriteLine();
 
